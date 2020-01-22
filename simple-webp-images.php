64f743064cd4b07251dd8f69bd0ddb7d0a68ed4d
Mike Bricknell-Barlow <mikeb@elementarydigital.co.uk>
2020-01-22 20:57:05 +0000

    adding settings link and buy-me-a-coffee link to plugins page

--- simple-webp-images.php
+++ simple-webp-images.php
@@ -3,7 +3,7 @@
  * Plugin Name:       Simple Webp Images
  * Plugin URI:        
  * Description:       Generates webp images from uploaded jpg or png images, and outputs webp images instead of jpg or png images in compatible browsers. Also optionally provides lazy-load functionality for converted images.
- * Version:           1.1.8
+ * Version:           1.1.9
  * Requires at least: 5.0.0
  * Requires PHP:      7.0
  * Author:            Mike Bricknell-Barlow
@@ -13,7 +13,7 @@
  * Text Domain:       simple-webp-images
 */
 
-define('SIMPLE_WEBP_IMAGES_VERSION', '1.1.8');
+define('SIMPLE_WEBP_IMAGES_VERSION', '1.1.9');
 define('SIMPLE_WEBP_IMAGES_PLUGIN_DIR_URL', plugin_dir_url(__FILE__));
 define('SIMPLE_WEBP_IMAGES_PLUGIN_DIR_PATH', dirname(__FILE__));
 
@@ -42,3 +42,17 @@ function swi_deactivation () {
     unlink ( WPMU_PLUGIN_DIR . '/output_buffering.php' );
 }
 register_deactivation_hook( __FILE__, 'swi_deactivation' );
+
+function swi_add_plugin_page_settings_link( $links ) {
+	$links[] = '<a href="' .
+		admin_url( 'options-general.php?page=simple-webp-images' ) .
+        '">' . __('Settings') . '</a>';
+        
+    $links[] = '<a href="' .
+		'https://www.buymeacoffee.com/ecHnK8Q' .
+        '">' . __('Buy me a coffee') . '</a>';
+    
+    return $links;
+}
+add_filter( 'plugin_action_links_' . plugin_basename(__FILE__), 'swi_add_plugin_page_settings_link' );
+
